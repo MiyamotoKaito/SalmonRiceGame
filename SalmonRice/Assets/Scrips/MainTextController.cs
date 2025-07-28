b6c204abf7628df0e7ba90f884398f5b2cdb302d
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-07-28 13:23:05 +0900

    <delete>StartUpdateの文を削除

--- SalmonRice/Assets/Scrips/MainTextController.cs
+++ SalmonRice/Assets/Scrips/MainTextController.cs
@@ -6,13 +6,10 @@ using UnityEngine;
 public class MainTextController : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _mainTextObject;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         DisPlayText();
     }
-
-    // Update is called once per frame
     void Update()
     {
        if (Input.GetMouseButtonDown(0))
