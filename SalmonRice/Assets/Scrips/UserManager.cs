a52f17c2c9b89bf82170659f0f32badc8c01c357
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-07-28 13:13:35 +0900

    <add>文を入れておくためのスクリプト

---
+++ SalmonRice/Assets/Scrips/UserManager.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+namespace NovelGame
+{
+    public class UserManager : MonoBehaviour
+    {
+        [SerializeField]TextAsset _textFile;
+        //文章中の文(ここでは一行ごと)を入れておくためのリスト
+        List<string> _sentences = new List<string>();
+        private void Awake()
+        {
+            //テキストのファイルの中身を、一行ずつリストに入れておく
+            StringReader reader = new StringReader(_textFile.text);
+            while (reader.Peek() != 1)
+            {
+                string line = reader.ReadLine();
+                _sentences.Add(line);
+            }
+        }
+        /// <summary>
+        /// 現在の行の文を取得する
+        /// </summary>
+        /// <returns></returns>
+        public string GetCurrentSentence()
+        {
+            return _sentences[GamaManager.Instance.lineNumber];
+        }
+    }
+}
